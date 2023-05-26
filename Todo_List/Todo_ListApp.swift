bfed625feeccca3af6b5e5023bfe552f7cf9c634
Cosmin Ghinea <cosmin.ghinea47@gmail.com>
2023-05-26 16:39:11 +0300

    Initial Commit

---
+++ Todo_List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo_List
+//
+//  Created by Cosmin Ghinea on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
